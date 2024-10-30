7b04419bf8113a596410b9e837e2eb4a22a195d5
Davidneo <davidneo@seznam.cz>
2024-10-30 23:32:17 +0100

    Kostra aplikace

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 //import { useState } from 'react'
 import './App.css'
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <div className="app">
+    <Header />
+    <main className="main-content">
+
+      {/* komponenty a obsah stránky */}
+
+    </main>
+    <Footer />
+  </div>
   )
 }
 
---
+++ src/components/footer.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+//import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© 2024 ElektroWytz</p>
+    </footer>
+  );
+};
+
+export default Footer;
